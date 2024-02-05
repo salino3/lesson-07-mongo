0ca0dda5b3aa53f7be357d21a63f0bc80885ae4f
salino3 <programacionflavio@gmail.com>
2024-02-05 11:20:59 +0100

    changes in console-runner

--- src/console-runners/queries.runner.ts
+++ src/console-runners/queries.runner.ts
@@ -1,10 +1,35 @@
+import { ObjectId } from 'mongodb';
 import { envConstants } from '#core/constants/index.js';
 import {
   connectToDBServer,
   disconnectFromDBServer,
 } from '#core/servers/index.js';
+import { getMovieContext } from '#dals/movie/movie.context.js';
 
-const runQueries = async () => {};
+const runQueries = async () => {
+  const result = await getMovieContext()
+    .findOneAndUpdate(
+      {
+        _id: new ObjectId('573a1390f29313caabcd4135'),
+      },
+      {
+        $pull: {
+          genres: 'Drama',
+        },
+      },
+      {
+        returnDocument: 'after',
+        projection: {
+          _id: 1,
+          title: 1,
+          genres: 1,
+          imdb: 1,
+          'tomatoes.viewer.rating': 1,
+        },
+      }
+    )
+  console.log({ result });
+};
 
 export const run = async () => {
   await connectToDBServer(envConstants.MONGODB_URI);
